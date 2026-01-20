bd8228b742bdcab89a95fe5c78b921e8ee05780b
Hamed Ghadirian <h.ghadirian@outlook.com>
2026-01-20 16:01:14 +0100

    Initial Commit

---
+++ NoCounter/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  NoCounter
+//
+//  Created by ghadirianh on 20.01.26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
